bbe3432562847e87857d9bf050d64a8a2ec75c88
Knugen23 <66279676+Knugen23@users.noreply.github.com>
2023-10-24 11:26:17 +0200

    Lagt til Kalibrasjon v0.1

---
+++ Python/Calibration/StereoImaging.py
@@ -0,0 +1,40 @@
+import cv2
+#cap1 is videocapture(0), cap2 is videocapture(2), this may differ from computer and port layout!
+cap1 = cv2.VideoCapture(0)
+cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap1.set(cv2.CAP_PROP_EXPOSURE, -1)
+
+cap2 = cv2.VideoCapture(2)
+cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap2.set(cv2.CAP_PROP_EXPOSURE, -1)
+
+if not cap1.isOpened() or not cap2.isOpened():
+    print("Error: Could not open camera.")
+
+    exit()
+while True:
+# Caputre frame from camera 1Left
+    ret1, frame1 = cap1.read()
+# Capture frame from camera 2Right
+    ret2, frame2 = cap2.read()
+
+    if not ret1 or not ret2:
+        print('Error: Could  not read one or both frames.')
+    #break
+    height, width, _ = frame1.shape
+    combined_frame = cv2.hconcat([frame1, frame2])
+
+    cv2.namedWindow("Left, Combined Images, Right", cv2.WINDOW_NORMAL)
+
+    cv2.imshow("Left, Combined Images, Right", combined_frame)
+   # cv2.imshow("Live Image1", frame1)
+   # cv2.imshow("Live Image2", frame2)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap1.release()
+cap2.release()
+
---
+++ Python/Calibration/calibration_images.py
@@ -0,0 +1,33 @@
+import cv2
+
+#cap1 is videocapture(0), cap2 is videocapture(2), this may differ from computer and port layout!
+cap1 = cv2.VideoCapture(0)
+cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap1.set(cv2.CAP_PROP_EXPOSURE, -1)
+
+cap2 = cv2.VideoCapture(2)
+cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap2.set(cv2.CAP_PROP_EXPOSURE, -1)
+
+num = 0
+
+while cap1.isOpened():
+
+    succes1, img1 = cap1.read() #img1 is Left camera
+    succes2, img2 = cap2.read() #img2 is Right camera
+
+    k = cv2.waitKey(5)
+
+    if k ==27:
+        break
+    elif k == ord('s'): # Wait for 's' key to save and exit.
+        cv2.imwrite('images/LeftStereo/ImageL' +str(num) + '.png', img1)
+        cv2.imwrite('images/RightStereo/ImageR' +str(num) + '.png', img2)
+        print('Images saved!')
+        num += 1
+
+
+    cv2.imshow('Left image', img1)
+    cv2.imshow('Right image', img2)
