a346c602b1b4ed19615badcad14d5f1dbefb9c2e
Knugen23 <66279676+Knugen23@users.noreply.github.com>
2023-11-04 01:23:40 +0100

    GUI2.0 update
    Nå er GUI2.0 kommet dit hen at den har fått tilbake 4bildene og ta snapshot

--- Python/GUI2.0.py
+++ Python/GUI2.0.py
@@ -43,7 +43,7 @@ class GUI():
 #        self.find_cameras()  # Call the find_cameras method to check available cameras
         
         # For dropdown menyen og de forskjellige typer masker som kan brukes for å finne hvem hører til hvor
-        image_output = StringVar(value='Original')
+        self.image_output = StringVar(value='Original')
         options = ['Original', 'Thresholding', 'Erosion', 'Dialation', 'Contour1', 'Contour2']
 
         # Create frames and labels
@@ -59,54 +59,54 @@ class GUI():
         self.centerFrame = tb.Frame(self.window, width = 100, height= 100)
         self.centerFrame.grid(row=0, column=1, padx=5, pady=5)
 
-        # calButton = tb.Button(centerFrame, text= 'Snapshot', command = snapShot, bootstyle = 'warning')
-        # calButton.grid(row=0, column=1, padx=5, pady=5)
+        calButton = tb.Button(self.centerFrame, text= 'Snapshot', command = self.snapShot, bootstyle = 'warning')
+        calButton.grid(row=0, column=1, padx=5, pady=5)
 
-        menuButton = tb.Menubutton(self.centerFrame,text='Original', bootstyle = "info")
-        menu =tb.Menu(menuButton)
+        self.menuButton = tb.Menubutton(self.centerFrame,text='Original', bootstyle = "info")
+        menu =tb.Menu(self.menuButton)
         for option in options:
-            menu.add_radiobutton(label=option, value=option, variable=image_output, command=lambda option=option:set_output(option))
-        menuButton['menu'] = menu
-        menuButton.grid(row=1, column=1, padx=5, pady=5)
+            menu.add_radiobutton(label=option, value=option, variable=self.image_output, command=lambda option=option:self.set_output(option))
+        self.menuButton['menu'] = menu
+        self.menuButton.grid(row=1, column=1, padx=5, pady=5)
 
-        satMinS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
-        satMinS.grid(row=2, column=1, padx=5, pady=0)
-        satMinL = tb.Label(self.centerFrame, text="0")
-        satMinL.grid(row=2, column=2, padx=2, pady=0)
-        satMinL2 = tb.Label(self.centerFrame, text="Saturation Min:")
-        satMinL2.grid(row=2, column=0, padx=2, pady=0)
-        satMaxS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
-        satMaxS.grid(row=3, column=1, padx=2, pady=0)
-        satMaxL = tb.Label(self.centerFrame, text="0")
-        satMaxL.grid(row=3, column=2, padx=2, pady=0)
-        satMaxL2 = tb.Label(self.centerFrame, text="Saturation Max:")
-        satMaxL2.grid(row=3, column=0, padx=2, pady=0)
+        # satMinS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
+        # satMinS.grid(row=2, column=1, padx=5, pady=0)
+        # satMinL = tb.Label(self.centerFrame, text="0")
+        # satMinL.grid(row=2, column=2, padx=2, pady=0)
+        # satMinL2 = tb.Label(self.centerFrame, text="Saturation Min:")
+        # satMinL2.grid(row=2, column=0, padx=2, pady=0)
+        # satMaxS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
+        # satMaxS.grid(row=3, column=1, padx=2, pady=0)
+        # satMaxL = tb.Label(self.centerFrame, text="0")
+        # satMaxL.grid(row=3, column=2, padx=2, pady=0)
+        # satMaxL2 = tb.Label(self.centerFrame, text="Saturation Max:")
+        # satMaxL2.grid(row=3, column=0, padx=2, pady=0)
 
-        hueMinS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
-        hueMinS.grid(row=4, column=1, padx=5, pady=0)
-        hueMinL = tb.Label(self.centerFrame, text="0")
-        hueMinL.grid(row=4, column=2, padx=2, pady=0)
-        hueMinL2 = tb.Label(self.centerFrame, text="Hue Min:")
-        hueMinL2.grid(row=4, column=0, padx=2, pady=0)
-        hueMaxS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
-        hueMaxS.grid(row=5, column=1, padx=2, pady=0)
-        hueMaxL = tb.Label(self.centerFrame, text="0")
-        hueMaxL.grid(row=5, column=2, padx=2, pady=0)
-        hueMaxL2 = tb.Label(self.centerFrame, text="Hue Max:")
-        hueMaxL2.grid(row=5, column=0, padx=2, pady=0)
+        # hueMinS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
+        # hueMinS.grid(row=4, column=1, padx=5, pady=0)
+        # hueMinL = tb.Label(self.centerFrame, text="0")
+        # hueMinL.grid(row=4, column=2, padx=2, pady=0)
+        # hueMinL2 = tb.Label(self.centerFrame, text="Hue Min:")
+        # hueMinL2.grid(row=4, column=0, padx=2, pady=0)
+        # hueMaxS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
+        # hueMaxS.grid(row=5, column=1, padx=2, pady=0)
+        # hueMaxL = tb.Label(self.centerFrame, text="0")
+        # hueMaxL.grid(row=5, column=2, padx=2, pady=0)
+        # hueMaxL2 = tb.Label(self.centerFrame, text="Hue Max:")
+        # hueMaxL2.grid(row=5, column=0, padx=2, pady=0)
 
-        valMinS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
-        valMinS.grid(row=6, column=1, padx=5, pady=0)
-        valMinL = tb.Label(self.centerFrame, text="0")
-        valMinL.grid(row=6, column=2, padx=2, pady=0)
-        valMinL2 = tb.Label(self.centerFrame, text="Value Min:")
-        valMinL2.grid(row=6, column=0, padx=2, pady=0)
-        valMaxS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
-        valMaxS.grid(row=7, column=1, padx=2, pady=0)
-        valMaxL = tb.Label(self.centerFrame, text="0")
-        valMaxL.grid(row=7, column=2, padx=2, pady=0)
-        valMaxL2 = tb.Label(self.centerFrame, text="Value Max:")
-        valMaxL2.grid(row=7, column=0, padx=2, pady=0)
+        # valMinS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
+        # valMinS.grid(row=6, column=1, padx=5, pady=0)
+        # valMinL = tb.Label(self.centerFrame, text="0")
+        # valMinL.grid(row=6, column=2, padx=2, pady=0)
+        # valMinL2 = tb.Label(self.centerFrame, text="Value Min:")
+        # valMinL2.grid(row=6, column=0, padx=2, pady=0)
+        # valMaxS = tb.Scale(self.centerFrame, from_= 0, to=255, command=self.scaler, bootstyle = 'primary')
+        # valMaxS.grid(row=7, column=1, padx=2, pady=0)
+        # valMaxL = tb.Label(self.centerFrame, text="0")
+        # valMaxL.grid(row=7, column=2, padx=2, pady=0)
+        # valMaxL2 = tb.Label(self.centerFrame, text="Value Max:")
+        # valMaxL2.grid(row=7, column=0, padx=2, pady=0)
 
         #Center frame end
 
@@ -117,10 +117,12 @@ class GUI():
         self.topImgR.grid(row=0, column=0)
         self.botImgR = tb.Label(self.rightFrame)
         self.botImgR.grid(row=1, column=0)
+    
+    def set_output(self, output):
+        self.image_output.set(output)
+        self.menuButton.config(text=output)
 
     def updateCameraFrames(self, frameL, frameR):
-        # retL, frameL = self.capL.read()
-        # retR, frameR = self.capR.read()
         if frameL is not None and frameR is not None:
             frameL = cv2.cvtColor(frameL, cv2.COLOR_BGR2RGB)
             frameR = cv2.cvtColor(frameR, cv2.COLOR_BGR2RGB)
@@ -135,13 +137,20 @@ class GUI():
             self.topImgR.configure(image=imageR)
             self.topImgR.image = imageR
 
+            self.botImgL.configure(image=imageL)
+            self.botImgL.image = imageL
+            self.botImgR.configure(image=imageR)
+            self.botImgR.image = imageR
+
         self.window.after(50, self.update)
     def update(self):
         frameL = self.capL.read()[1]
         frameR = self.capR.read()[1]
+        self.set_output(self.image_output)
+
         
         # Pass frameL and frameR to Colorconverter when creating an instance
-        self.myColorconverter = Colorconverter(frameL, frameR) 
+        # self.myColorconverter = Colorconverter(frameL, frameR) 
 
         self.updateCameraFrames(frameL, frameR)
 
@@ -160,25 +169,25 @@ class GUI():
         except Exception as e:
             print("Error:", str(e))
     
-    def scaler(self, e):
-        print("Scaler function called")
-        print(f"satMinL: {self.myColorconverter.satMinL}, satMaxL: {self.myColorconverter.satMaxL}, hueMinL: {self.myColorconverter.hueMinL}, hueMaxL: {self.myColorconverter.hueMaxL}, valMinL: {self.myColorconverter.valMinL}, valMaxL: {self.myColorconverter.valMaxL}")
+    # def scaler(self, e):
+    #     print("Scaler function called")
+    #     print(f"satMinL: {self.myColorconverter.satMinL}, satMaxL: {self.myColorconverter.satMaxL}, hueMinL: {self.myColorconverter.hueMinL}, hueMaxL: {self.myColorconverter.hueMaxL}, valMinL: {self.myColorconverter.valMinL}, valMaxL: {self.myColorconverter.valMaxL}")
 
 
-        self.myColorconverter.satMinL = satMinS.get()
-        self.myColorconverter.satMaxL = satMaxS.get()
-        satMinL.config(text=f'{int(satMinS.get())}')
-        satMaxL.config(text=f'{int(satMaxS.get())}')
+    #     self.myColorconverter.satMinL = satMinS.get()
+    #     self.myColorconverter.satMaxL = satMaxS.get()
+    #     satMinL.config(text=f'{int(satMinS.get())}')
+    #     satMaxL.config(text=f'{int(satMaxS.get())}')
 
-        self.myColorconverter.hueMinL = hueMinS.get()
-        self.myColorconverter.hueMaxL = hueMaxS.get()
-        hueMinL.config(text=f'{int(hueMinS.get())}')
-        hueMaxL.config(text=f'{int(hueMaxS.get())}')
+    #     self.myColorconverter.hueMinL = hueMinS.get()
+    #     self.myColorconverter.hueMaxL = hueMaxS.get()
+    #     hueMinL.config(text=f'{int(hueMinS.get())}')
+    #     hueMaxL.config(text=f'{int(hueMaxS.get())}')
 
-        self.myColorconverter.valMinL = valMinS.get()
-        self.myColorconverter.valMaxL = valMaxS.get()
-        valMinL.config(text=f'{int(valMinS.get())}')
-        valMaxL.config(text=f'{int(valMaxS.get())}')
+    #     self.myColorconverter.valMinL = valMinS.get()
+    #     self.myColorconverter.valMaxL = valMaxS.get()
+    #     valMinL.config(text=f'{int(valMinS.get())}')
+    #     valMaxL.config(text=f'{int(valMaxS.get())}')
     
     # def find_cameras(self, max_cameras_to_check=10):
     #     self.available_cameras = []
@@ -195,54 +204,54 @@ class GUI():
 
     def run(self):
         self.bildenr = 0
-#        self.find_cameras()
-#        self.update()
+        # self.find_cameras()
+        self.update()
         self.window.mainloop()
 
-class Colorconverter():
-    def __init__(self, frameL, frameR) -> None:
-        self.frameL = frameL
-        self.frameR = frameR
-        self.hueMinL = 175
-        self.hueMaxL = 25
-        self.satMinL = 160
-        self.satMaxL = 255
-        self.valMinL = 150
-        self.valMaxL = 255
-        self.erodeL = None
-        self.erodeR = None
-        self.maskL = None
-        self.maskR = None
-        self.hsvL = cv2.cvtColor(frameL, cv2.COLOR_BGR2HSV)
-        self.hsvR = cv2.cvtColor(frameR, cv2.COLOR_BGR2HSV)
+# class Colorconverter():
+#     def __init__(self, frameL, frameR) -> None:
+#         self.frameL = frameL
+#         self.frameR = frameR
+#         self.hueMinL = 175
+#         self.hueMaxL = 25
+#         self.satMinL = 160
+#         self.satMaxL = 255
+#         self.valMinL = 150
+#         self.valMaxL = 255
+#         self.erodeL = None
+#         self.erodeR = None
+#         self.maskL = None
+#         self.maskR = None
+#         self.hsvL = cv2.cvtColor(frameL, cv2.COLOR_BGR2HSV)
+#         self.hsvR = cv2.cvtColor(frameR, cv2.COLOR_BGR2HSV)
 
-    def create_mask(self):
-        print(f"satMinLmask: {self.satMinL}, satMaxLmask: {self.satMaxL}, hueMinLmask: {self.hueMinL}, hueMaxLmask: {self.hueMaxL}, valMinLmask: {self.valMinL}, valMaxLmask: {self.valMaxL}")
-        if self.hueMinL > self.hueMaxL:
-            maskl = cv2.inRange(self.hsvL, (0, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
-            maskh = cv2.inRange(self.hsvL, (self.hueMinL, self.satMinL, self.valMinL), (255, self.satMaxL, self.valMaxL))
-            self.maskL = cv2.bitwise_or(maskl, maskh)
+#     def create_mask(self):
+#         print(f"satMinLmask: {self.satMinL}, satMaxLmask: {self.satMaxL}, hueMinLmask: {self.hueMinL}, hueMaxLmask: {self.hueMaxL}, valMinLmask: {self.valMinL}, valMaxLmask: {self.valMaxL}")
+#         if self.hueMinL > self.hueMaxL:
+#             maskl = cv2.inRange(self.hsvL, (0, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
+#             maskh = cv2.inRange(self.hsvL, (self.hueMinL, self.satMinL, self.valMinL), (255, self.satMaxL, self.valMaxL))
+#             self.maskL = cv2.bitwise_or(maskl, maskh)
 
-            maskl = cv2.inRange(self.hsvR, (0, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
-            maskh = cv2.inRange(self.hsvR, (self.hueMinL, self.satMinL, self.valMinL), (255,self.satMaxL, self.valMaxL))
-            self.maskR = cv2.bitwise_or(maskl, maskh)
-        else:
-            self.maskL = cv2.inRange(self.hsvL, (self.hueMinL, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
-            self.maskR = cv2.inRange(self.hsvR, (self.hueMinL, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
+#             maskl = cv2.inRange(self.hsvR, (0, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
+#             maskh = cv2.inRange(self.hsvR, (self.hueMinL, self.satMinL, self.valMinL), (255,self.satMaxL, self.valMaxL))
+#             self.maskR = cv2.bitwise_or(maskl, maskh)
+#         else:
+#             self.maskL = cv2.inRange(self.hsvL, (self.hueMinL, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
+#             self.maskR = cv2.inRange(self.hsvR, (self.hueMinL, self.satMinL, self.valMinL), (self.hueMaxL, self.satMaxL, self.valMaxL))
 
-    def erode_mask(self):
-            self.create_mask()
-            kernel = np.ones((10,10), np.uint8)
-            self.maskL = cv2.threshold(self.maskL, 1, 255, cv2.THRESH_BINARY)[1]
-            self.maskR = cv2.threshold(self.maskR, 1, 255, cv2.THRESH_BINARY)[1]
-            self.erodeL = cv2.erode(self.maskL, kernel, iterations=1)
-            self.erodeR = cv2.erode(self.maskR, kernel, iterations=1)
-            print('erodeR')
+#     def erode_mask(self):
+#             self.create_mask()
+#             kernel = np.ones((10,10), np.uint8)
+#             self.maskL = cv2.threshold(self.maskL, 1, 255, cv2.THRESH_BINARY)[1]
+#             self.maskR = cv2.threshold(self.maskR, 1, 255, cv2.THRESH_BINARY)[1]
+#             self.erodeL = cv2.erode(self.maskL, kernel, iterations=1)
+#             self.erodeR = cv2.erode(self.maskR, kernel, iterations=1)
+#             print('erodeR')
 
-    def dilate_mask(self):
-            kernel = np.ones((5,5), np.uint8)
+#     def dilate_mask(self):
+#             kernel = np.ones((5,5), np.uint8)
 
-            self.dilateL = cv2.dilate(self.erodeL, kernel, iterations=1)
-            self.dilateR = cv2.dilate(self.erodeR, kernel, iterations=1)
+#             self.dilateL = cv2.dilate(self.erodeL, kernel, iterations=1)
+#             self.dilateR = cv2.dilate(self.erodeR, kernel, iterations=1)
 app = GUI()
 app.run()
---
+++ Python/Tkinter UI /Pertest.py
@@ -0,0 +1,67 @@
+import cv2
+import tkinter as tk
+from tkinter import ttk
+from ttkbootstrap import Style
+from PIL import Image, ImageTk
+import numpy as np
+
+class CameraViewer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Camera Viewer")
+
+        # Create two frames to display camera feeds
+        self.frame1 = ttk.Frame(self.root)
+        self.frame1.pack(side=tk.LEFT, padx=10, pady=10)
+        self.frame2 = ttk.Frame(self.root)
+        self.frame2.pack(side=tk.RIGHT, padx=10, pady=10)
+
+        # Initialize OpenCV video capture for two cameras
+        self.cap1 = cv2.VideoCapture(0)
+        self.cap2 = cv2.VideoCapture(2)
+
+        # Create labels for camera feed displays
+        self.label1 = ttk.Label(self.frame1, text="Camera 1")
+        self.label1.pack()
+        self.label2 = ttk.Label(self.frame2, text="Camera 2")
+        self.label2.pack()
+
+        # Start a loop to continuously update camera feeds
+        self.update()
+
+    def update(self):
+        ret1, frame1 = self.cap1.read()
+        ret2, frame2 = self.cap2.read()
+
+        if ret1 and ret2:
+            # Convert OpenCV BGR frames to RGB
+            frame1_rgb = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+            frame2_rgb = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
+
+            # Convert frames to PIL Image
+            image1 = Image.fromarray(frame1_rgb)
+            image2 = Image.fromarray(frame2_rgb)
+
+            # Convert PIL Images to Tkinter PhotoImage
+            photo1 = ImageTk.PhotoImage(image=image1)
+            photo2 = ImageTk.PhotoImage(image=image2)
+
+            # Update labels with new frames
+            self.label1.config(image=photo1)
+            self.label1.image = photo1
+            self.label2.config(image=photo2)
+            self.label2.image = photo2
+
+        # Schedule the update method to run again after 10ms
+        self.root.after(10, self.update)
+
+    def __del__(self):
+        # Release camera resources when the application is closed
+        self.cap1.release()
+        self.cap2.release()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    style = Style(theme='superhero')
+    app = CameraViewer(root)
+    root.mainloop()
